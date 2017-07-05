edf114d5b7495093e30b82508972f0d48995af06
Jacob Sherman <jacob@jbsherman.com>
2017-07-05 10:14:40 -0400

    Fea: getAll returns books

--- src/App.js
+++ src/App.js
@@ -11,12 +11,17 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
+     books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(
+      (books) => {this.setState( {books:books} )})
   }
 
   render() {
     return (
       <div className="app">
-
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
