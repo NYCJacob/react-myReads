a6afec6d1e008d079dab405371fbb5773dfc9e32
Jacob Sherman <jacob@jbsherman.com>
2017-07-08 18:21:45 -0400

    Fea: installed BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import BooksApp from './App'
 import './index.css'
 
 ReactDOM.render(
-  <BooksApp />, document.getElementById('root')
+  <BrowserRouter><BooksApp /></BrowserRouter>, 
+  document.getElementById('root')
 )
